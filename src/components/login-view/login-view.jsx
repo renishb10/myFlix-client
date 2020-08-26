11232a0f51d5c9a4330b4716549b1e8f380d176f
Renish B <renishb10@gmail.com>
2020-08-26 14:28:10 +0530

    added bootstrap design for all views

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,26 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import PropTypes from 'prop-types';
 
-export function LoginView(props) {
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
-  
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+
+import "./login-view.scss";
+
+export const LoginView = (props) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleSubmit = () => {
+  //e.preventDefault();
     console.log(username, password);
-    /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
-  };
-  
+   props.onLoggedIn(username, password);
+  }
+
+
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <button type="button" onClick={handleSubmit}>Submit</button>
-    </form>
-  );
+    <Container className="login-form">
+      <Form>
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+          <Form.Text className="text-muted">
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="current-password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={handleSubmit}>Log In</Button>
+        <Form.Text className="text-muted">
+          New user?<a href="#" onClick={() => props.onClick()}>click here</a>
+        </Form.Text>
+      </Form>
+    </Container>
+  )
+}
+
+LoginView.propTypes = {
+  onLoggedIn: PropTypes.func.isRequired
 }
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export class MovieCard extends React.Component {
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+/*sources of all Individual files and folders*/
+import "./movie-card.scss";
+
+export class MovieCard extends Component {
   render() {
-    const { movie, onClick } = this.props;
+    /* This is given to the <MovieCard/> component by the outer world which, in this case, is `MainView`, */
+    /* as `MainView` is what’s connected to your database via the movies endpoint of your API */
+    const { movie, click } = this.props;
 
     return (
-      <div onClick={() => onClick(movie)} className="movie-card">{movie.Title}</div>
+      <Card className="mb-3 mb-sm-4" style={{ width: '16rem' }}>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Button onClick={() => click(movie)} variant="link">Open</Button>
+        </Card.Body>
+      </Card>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Year: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImageURL: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string,
+      Description: PropTypes.string
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string,
+      Description: PropTypes.string,
+      Birth: PropTypes.string
+    }),
+    Featured: PropTypes.bool
+  }).isRequired,
+    Actors:PropTypes.string,
+    click: PropTypes.func.isRequired
+};
